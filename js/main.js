d00fb0e83181c05acaff7bc16711420029bcdea8
Andrew Andreev <andrey.andreev@fogstream.ru>
2018-06-02 16:02:58 +1000

    add nebPay.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+var NebPay = require('nebpay');
+
 (function($) {
-    'use strict';
+    var dappAddress = 'n1vrq258dYUwe8Y5RqffimwzDM2nv5CFjEF';
+    var serialNumber = null;
+    var nebPay = new NebPay();
 
     function onReadFileEndHandler(event) {
         var buffer = event.target.result;
         var digest = sha256(buffer);
         $('#fileDigest').val(digest);
-        var $btnSubmit = $('button:submit');
+        var $btnSubmit = $('#saveDigestBtn');
         $btnSubmit.removeClass('disabled');
         $btnSubmit.attr('disabled', false);
     }
@@ -22,11 +26,25 @@
         reader.readAsArrayBuffer(file);
     }
 
+    function saveDigestListener(response) {
+    }
+
     $('#certifiedFile').on('change', function(event) {
         if(this.files == undefined || this.files.length == 0) {
             return;
         }
         var file = this.files[0];
         readFile(file);
+    });
+
+    $('#saveDigestBtn').on('click', function () {
+        var args = JSON.stringify([$('#fileDigest').val()]);
+        var options = {
+            qrcode: {
+                showQRCode: false
+            },
+            listener: saveDigestListener,
+        }
+        serialNumber = nebPay.call(dappAddress, 0, 'set', args, options);
     });
 }(jQuery));
